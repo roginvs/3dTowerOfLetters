8c23f05d58b4efcb253552d0927e9f38a8cb2bcd
Vasilii Rogin <vasya@rogin.ru>
2016-07-30 13:54:41 +0200

    Trying

--- make.sh
+++ make.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo `date`" Running openscad"
 openscad -o TowerOfLetters.stl TowerOfLetters.scad
-ecgi `date`" Done"
+echo `date`" Done"
+
+echo 'Render done at '`date` | mail -s 'Openscad render finished' vasya@rogin.ru
